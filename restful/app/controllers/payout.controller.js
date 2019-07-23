3771cc3cce327957f10a79872816cfc8e08afa12
tzfeng <timothyzhfeng@gmail.com>
2019-07-23 18:20:02 +0800

    Update payout.controller.js

--- restful/app/controllers/payout.controller.js
+++ restful/app/controllers/payout.controller.js
@@ -2,16 +2,21 @@
 const scrape = require('../scrape/scraper.ts');
 const sc = require('../sc/sc2.js');
 const sched = require('node-schedule');
+const moment = require('moment');
+const Bet = require('../controllers/bet.model.js')
 
 exports.init = async function(req, res) {
 
-	var payout_check = schedule.scheduleJob('16 * * * * ', () => {
-		const now = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 16, 0, 0, 0);
+	var payout_check = schedule.scheduleJob('30 0 0 * * *', () => {
+		const now = moment().format('YYYY-MM-DD');
 
 		// for everything in database with exp date now
-		payout_bets = db.collection.find({ date: now });
+		payout_bets = Bet.find({ date: now }, (err, bets) => {
+			if (err) return console.error(err);
+			console.log(bets);
+		});
 		for (let bet in payout_bets) {
-			id = bet['betId'];
+			id = bet['betID'];
 			ticker = bet['ticker'];
 
 			let current_price = -1;
@@ -30,15 +35,17 @@ exports.init = async function(req, res) {
 	        } catch (error) {
 	            return response.status(400).send(error);
 	        }
-
-	        db.collection.deleteOne({betId : id});
 		}
+		Bet.deleteMany(bets);
 
-		vote_bets = db.collection.find({vote_date: now});
-		for (let bet in vote_bets) {
+		// vote_bets = Bet.find({vote_date: now}, (err, bets) => {
+		// 	if (err) return console.error(err);
+		// 	console.log(bets);
+		// });
+		// for (let bet in vote_bets) {
 			// do something to remove voting functionality
 			// maybe display that the voting period has elapsed on frontend
-		}
+	// 	}
 
 	});
 
