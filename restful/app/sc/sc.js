be4af87c2836ac3ed135c6cf44e6c1654ba5f2b0
tzfeng <timothyzhfeng@gmail.com>
2019-07-24 14:29:03 +0800

    Update sc.js

--- restful/app/sc/sc.js
+++ restful/app/sc/sc.js
@@ -101,6 +101,94 @@ exports.payout = async function(req, res) {
 
 }
 
+exports.record = async function(req, res) {
+    
+    const restClient = new Ont.RestClient();
+
+    const privateKey = new Ont.Crypto.PrivateKey('6ac187607c14403ae96327137b230349eebd8693bd2a7be6b63da3449598bb63');
+    const account = Ont.Account.create(privateKey, 'l', 'test');
+    // console.log(account.address.serialize());
+
+    const contract = Ont.utils.reverseHex('e6c6a65757950ccb73c5b9791cbf4e12dc8203b8');
+    const contractAddr = new Ont.Crypto.Address(contract);
+    const method = 'user_record';
+
+    const params = [
+                new Parameter('Address', ParameterType.String, req[0])
+                ];
+
+    // console.log(JSON.stringify(params));
+
+    const tx = Ont.TransactionBuilder.makeInvokeTransaction(method, params, contractAddr, '500', '27026', account.address);
+    Ont.TransactionBuilder.signTransaction(tx, privateKey);
+
+    try {
+    txnString = await restClient.sendRawTransaction(tx.serialize(), false, true); // 2nd arg is if you read it 
+    txn = txnString["Result"];
+    console.log(JSON.stringify(txn));
+    res = txn;
+    // return res;
+    }
+
+    catch (e) { throw e; }
+
+    try {
+    var val = await sync_block.syncRecord(txn);
+    // console.log("val ? " + JSON.stringify(val));
+    } 
+    catch (e) {
+        console.log(e);
+    }
+    
+    return val;
+    // const res2 = await restClient.getRawTransaction(res["Result"]);
+    // console.log(res2);
+
+}
+
+exports.feed = async function(req, res) {
+    
+    const restClient = new Ont.RestClient();
+
+    const privateKey = new Ont.Crypto.PrivateKey('6ac187607c14403ae96327137b230349eebd8693bd2a7be6b63da3449598bb63');
+    const account = Ont.Account.create(privateKey, 'l', 'test');
+    // console.log(account.address.serialize());
+
+    const contract = Ont.utils.reverseHex('e6c6a65757950ccb73c5b9791cbf4e12dc8203b8');
+    const contractAddr = new Ont.Crypto.Address(contract);
+    const method = 'feed';
+
+    const params = [];
+
+    // console.log(JSON.stringify(params));
+
+    const tx = Ont.TransactionBuilder.makeInvokeTransaction(method, params, contractAddr, '500', '27026', account.address);
+    Ont.TransactionBuilder.signTransaction(tx, privateKey);
+
+    try {
+    txnString = await restClient.sendRawTransaction(tx.serialize(), false, true); // 2nd arg is if you read it 
+    txn = txnString["Result"];
+    console.log(JSON.stringify(txn));
+    res = txn;
+    // return res;
+    }
+
+    catch (e) { throw e; }
+
+    try {
+    var val = await sync_block.syncFeed(txn);
+    // console.log("val ? " + JSON.stringify(val));
+    } 
+    catch (e) {
+        console.log(e);
+    }
+    
+    return val;
+    // const res2 = await restClient.getRawTransaction(res["Result"]);
+    // console.log(res2);
+
+}
+
 let params = [ "Acuj41fD4MdubYa8FmEMtYHS9eD2AxnXF5",
   1,
   "FB",
@@ -110,4 +198,3 @@ let params = [ "Acuj41fD4MdubYa8FmEMtYHS9eD2AxnXF5",
   1];
 
 // create_bet(params, null).then((ans)=> {console.log("out > " + ans)});
-
