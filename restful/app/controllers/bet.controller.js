5d9f8159f877cbb9125c73eb35fa391b374384aa
tzfeng <timothyzhfeng@gmail.com>
2019-07-25 16:33:22 +0800

    Update bet.controller.js

--- restful/app/controllers/bet.controller.js
+++ restful/app/controllers/bet.controller.js
@@ -1,227 +1,177 @@
-const axios = require('axios');
-const config = require('./config.js');
-const Ont = require('ontology-ts-sdk');
-const BigNumber = require('bignumber.js');
-const moment = require('moment');
+const Bet = require('../models/bet.model.js');
+const sc = require('../sc/sc.js');
+const scrape = require('../scrape/scraper.ts');
+const sync_block = require('../sync/sync_block.js');
 
-const restfulUrl = config.url + ":20334";
+// const sync = require('../../sync/sync_block.js');
+// const sc = require('../../node_modules/ontology-ts-sdk/')
 
-function HexToInt(num) {
-    return parseInt('0x'+num.match(/../g).reverse().join(''));
-}
+// Create and Save a new bet
+exports.create = async function(req, res) {
 
-const fetchLastBlock = async function () {
-    const url = restfulUrl + '/api/v1/block/height';
-    const res = await axios.get(url);
-    // console.log(res.data.Result);
-    var height;
-    if(res && res.data && res.data.Result) {
-        height = res.data.Result;
-    } else {
-        height = 0;
+	// Validate request
+    if(!req.body.address) {
+        return res.status(400).send({
+            message: "bet add can not be empty"
+        });
     }
-    return height;
-}
 
-const fetchScEventsByBlock = async function (height) {
-    const url = restfulUrl + '/api/v1/smartcode/event/transactions/' + height;
-    // console.log(url);
-    const res = await axios.get(url);
-    if(!res || !res.data) {
-        return;
+    // scrape price
+    let init_price1 = -1;
+    try { init_price1 = 11; // await scrape.scrapePrice("AAPL");
+    //console.log(init_price1);
     }
-    const txs = res.data.Result;
-    if (txs!= "") {
-        // console.log('txs are : ' + JSON.stringify(txs));
+    catch (error) {
+        return res.status(400).send("scraper died");
+    }
+
+    // scrape sector
+    let sect1 = "unrendered";
+    try { sect1 = "strings"; // await scrape.scrapeSect("AAPL");
+    //console.log(sect1);
+    }
+    catch (error) {
+        return res.status(400).send("scraper died");
     }
     
-    const events = []
-    for (const tx of txs) {
-        for (const notify of tx.Notify) {
-            if (notify.ContractAddress === config.myContractHash) {
-                notify.TxHash = tx['TxHash']
-                events.push(notify)
-                // break;
-            }
+    // SC
+       const params = [ req.body.address,
+          req.body.amount_staked,
+          req.body.ticker,
+          req.body.sign,
+          req.body.margin,
+          req.body.date,
+          init_price1];
+   
+    let val = await sc.create_bet(params, null);
+    console.log(JSON.stringify(val));
+
+    // Create a bet
+    const bet = new Bet({
+        title: "oh no", 
+        _id: Number(val[0]) || -404,
+        ticker: val[1] || 'Invalid ticker',
+        change: Number(val[2]) || -404,
+        target_price: Number(val[3]) || -404,
+        date: val[4] || 'Invalid date',
+        for_staked: Number(val[5]),
+        against_staked: Number(val[6]),
+        for_avg_rep: Number(val[7]),
+        against_avg_rep: Number(val[8]),
+        prob: Number(val[9]),
+        sector: sect1
+    });
+
+    // Save bet in the database
+    bet.save()
+    .then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while creating the bet."
+        });
+    });
+
+};
+
+// Retrieve and return all bets from the database.
+exports.findAll = (req, res) => {
+    Bet.find()
+    .then(bets => {
+        res.send(bets);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while retrieving bets."
+        });
+    });
+};
+
+// Find a single bet with a betId
+exports.findOne = (req, res) => {
+    Bet.findById(req.params.betId)
+    .then(bet => {
+        if(!bet) {
+            return res.status(404).send({
+                message: "bet not found with id " + req.params.betId
+            });            
         }
-    }
-    return events;
-}
-
-exports.syncBet = async function (current_hash) {
-
-    const height = await fetchLastBlock();
-    
-    const notifyList = await fetchScEventsByBlock(height);
-    
-
-    if (notifyList.length > 0) {
-        for (const notify of notifyList) {
-            const states = notify['States'];
-            const contractHash = notify['ContractAddress']; 
-            // console.log("\n*** the whole notify is " + JSON.stringify(notify));
-            if (contractHash == config.myContractHash) {
-                if (notify.TxHash == current_hash) {
-                    const key = Ont.utils.hexstr2str(states[0]);
-                    if (key == 'bet') {
-                        // console.log(JSON.stringify(states[2][9]));
-                        const bet = HexToInt(states[1]);
-                        // const for_rep = HexToInt(states[2][0]);
-                        // const against_rep = HexToInt(states[2][1]);                 
-                        const for_staked = HexToInt(states[2][2]) / 1e8;
-                        const against_staked = HexToInt(states[2][3]) / 1e8;
-                        const change = HexToInt(states[2][6]) * HexToInt(states[2][7]);
-                        const target_price = HexToInt(states[2][8]) / 1e8;
-                        const date = Ont.utils.hexstr2str(states[2][9]);
-                        const ticker = Ont.utils.hexstr2str(states[2][10]);
-                        const for_avg_rep = HexToInt(states[2][11]) / 1e8 - 1e8;
-                        var against_avg_rep;
-                        if (against_staked == 0) { against_avg_rep = 0; }
-                        else { against_avg_rep = HexToInt(states[2][12]) / 1e8 - 1e8; }
-                        const prob = HexToInt(states[2][13]) / 1e8;
-
-                        const val = [bet, ticker, change, target_price, date, for_staked, against_staked, for_avg_rep, against_avg_rep, prob];
-                        return val;
-                    }
-                }                 
-            }          
+        res.send(bet);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "bet not found with id " + req.params.betId
+            });                
         }
+        return res.status(500).send({
+            message: "Error retrieving bet with id " + req.params.betId
+        });
+    });
+};
+
+// Update a bet identified by the betId in the request
+exports.update = async function(req, res) {
+    // Validate Request
+    if(!req.body.betId) {
+        return res.status(400).send({
+            message: "bet address can not be empty"
+        });
     }
-}
 
-exports.syncVote = async function (current_hash) {
 
-    const height = await fetchLastBlock();
-    
-    const notifyList = await fetchScEventsByBlock(height);
-    
+    const params = [ req.body.betId,
+    req.body.address,
+    req.body.amount_staked,
+    req.body.for_against ];
 
-    if (notifyList.length > 0) {
-        for (const notify of notifyList) {
-            const states = notify['States'];
-            const contractHash = notify['ContractAddress']; 
-            // console.log("\n*** the whole notify is " + JSON.stringify(notify));
+    let val = await sc.vote(params, null);
+    console.log(JSON.stringify(val));
+    console.log(req.params.betId);
 
-            if (contractHash == config.myContractHash) {
-                if (notify.TxHash == current_hash) {
-                    const key = Ont.utils.hexstr2str(states[0]);
-                    if (key == 'vote') {
-                        // console.log(JSON.stringify(states[2][9]));
-                            const bet = HexToInt(states[1]);
-                        // const for_rep = HexToInt(states[2][0]);
-                        // const against_rep = HexToInt(states[2][1]);                 
-                        const for_staked = HexToInt(states[2][2]) / 1e8;
-                        const against_staked = HexToInt(states[2][3]) / 1e8;
-                        const change = HexToInt(states[2][6]) * HexToInt(states[2][7]);
-                        const target_price = HexToInt(states[2][8]) / 1e8;
-                        const date = Ont.utils.hexstr2str(states[2][9]);
-                        const ticker = Ont.utils.hexstr2str(states[2][10]);
-                        const for_avg_rep = HexToInt(states[2][11]) / 1e8 - 1e8;
-                        var against_avg_rep;
-                        if (against_staked == 0) { against_avg_rep = 0; }
-                        else { against_avg_rep = HexToInt(states[2][12]) / 1e8 - 1e8; }
-                        const prob = HexToInt(states[2][13]) / 1e8;
-
-                        const val = [bet, ticker, change, target_price, date, for_staked, against_staked, for_avg_rep, against_avg_rep, prob];
-                        return val;
-                    }
-                }                 
-            }          
+    // Find bet and update it with the request body
+    Bet.findByIdAndUpdate(req.params.betId, {
+        for_staked: Number(val[5]),
+        against_staked: Number(val[6]),
+        for_avg_rep: Number(val[7]),
+        against_avg_rep: Number(val[8]),
+        prob: Number(val[9]),
+    }, {new: true})
+    .then(bet => {
+        if(!bet) {
+            return res.status(404).send({
+                message: "bet not found with id " + req.params.betId
+            });
         }
-    }
-}
-
-exports.syncFeed = async function (current_hash) {
-
-    const height = await fetchLastBlock();
-    
-    try { 
-        var notifyList = await fetchScEventsByBlock(height);
-    }
-    catch (e) { throw e };
-    
-    const betEvents = [];
-
-    if (notifyList.length > 0) {
-        for (const notify of notifyList) {
-            const states = notify['States'];
-            const contractHash = notify['ContractAddress']; 
-            // console.log("\n*** the whole notify is " + JSON.stringify(notify));
-
-            
-            if (notify.TxHash === current_hash) {
-                const key = Ont.utils.hexstr2str(states[0]);
-                if (key == 'feed') {
-                    var map = new Map();
-
-                    for (let i = 0; i < states[1].length; i++) {
-                        // console.log(JSON.stringify(states[1][i]));
-                        const bet = HexToInt(states[1][i][0]);
-                        const ticker = Ont.utils.hexstr2str(states[1][i][1], 16);
-
-                        console.log("raw " + states[1][i][2]);
-                        console.log("after parse " + HexToInt(states[1][i][2]));
-
-                        const target_price = HexToInt(states[1][i][2]) / 1e8;
-
-                        const change = parseInt(states[1][i][3], 16) * parseInt(states[1][i][4], 16);
-
-
-                        const for_avg_rep = (HexToInt(states[1][i][5]) / 1e8) - 1e8;
-
-                        const for_staked = HexToInt(states[1][i][7]) / 1e8;
-                        const against_staked = HexToInt(states[1][i][8]) / 1e8;
-
-
-                        const against_avg_rep = (HexToInt(states[1][i][6]) / 1e8) - 1e8;
-                        if (against_staked == 0) { against_avg_rep = 0; }
-
-                        const date = Ont.utils.hexstr2str(states[1][i][9], 16)
-                        const prob = HexToInt(states[1][i][10]) / 1e8;
-
-                        const val = [ticker, target_price, change, for_avg_rep, against_avg_rep, for_staked, against_staked, date, prob];
-                        map.set(bet, val);
-                        }
-                        
-
-                    return map;
-                }
-            }  
-                      
+        res.send(bet);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "bet not found with id " + req.params.betId
+            });                
         }
-    }
-}
+        return res.status(500).send({
+            message: "Error updating bet with id " + req.params.betId
+        });
+    });
+};
 
-exports.syncRecord = async function (current_hash) {
-
-    const height = await fetchLastBlock();
-    
-    const notifyList = await fetchScEventsByBlock(height);
-
-    if (notifyList.length > 0) {
-        for (const notify of notifyList) {
-            // e means each event 
-            const states = notify['States'];
-            const contractHash = notify['ContractAddress']; 
-            // console.log("\n*** the whole notify is " + JSON.stringify(notify));
-
-            if (contractHash == config.myContractHash) {
-                if (notify.TxHash === current_hash) {
-                    const key = Ont.utils.hexstr2str(states[0]);
-                        if (key == 'record') {
-                        const bets = states[1];
-                        const results = states[2];
-                        const net = states[3];
-                        for (let i = 0; i < bets.length; i++) {
-                            bets[i] = parseInt(bets[i], 16);
-                            results[i] = parseInt(results[i], 16);
-                            net[i] = parseInt(net[i], 16);
-                        }
-                        const val = [bets, results, net];
-                        return val; 
-                    }
-                }  
-            }          
+// Delete a bet with the specified betId in the request
+exports.delete = (req, res) => {
+    bet.findByIdAndRemove(req.params.betId)
+    .then(bet => {
+        if(!bet) {
+            return res.status(404).send({
+                message: "bet not found with id " + req.params.betId
+            });
         }
-    }
-}
+        res.send({message: "bet deleted successfully!"});
+    }).catch(err => {
+        if(err.kind === 'ObjectId' || err.name === 'NotFound') {
+            return res.status(404).send({
+                message: "bet not found with id " + req.params.betId
+            });                
+        }
+        return res.status(500).send({
+            message: "Could not delete bet with id " + req.params.betId
+        });
+    });
+};
